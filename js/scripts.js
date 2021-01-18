b213da891f78411016b1e126963f2485cc2a64eb
Kelly Young <kkristineyoung@gmail.com>
2021-01-18 16:07:20 -0500

    add Contact constructor and prototype method

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,9 @@
-$(document).ready(function() {
+function Contact(firstName, lastName, phoneNumber) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.phoneNumber = phoneNumber;
+}
 
-});
+Contact.protoype.fullName = function() {
+  return this.firstName + " " + this.lastName;
+}
