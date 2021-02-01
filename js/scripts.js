2d5de637ee5eb24eb49e71317ce39a185e6ef867
Kelly Young <kkristineyoung@gmail.com>
2021-01-31 22:08:40 -0600

    Adds ability to click new/list and view the form if you click new

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,40 @@
-// Business Logic
+// // Business Logic
+// let firstName = $("#firstName").val();
+// let lastName = $("#lastName").val();
 
+// // Assembling full name
+// function fullName(firstName, lastName){
+//   this.firstName = firstName;
+//   this.lastName = lastName;
+// }
 
+// // email
+// let email = $("#email").val();
 
+// // Address components from survey input
+// let addressStreet = $("#addressStreet").val();
+// let city = $("#city").val();
+// let state = $("#state").val();
+// let zip = $("#zip").val();
 
+// // Assembling address from address input
+// function Address(addressStreet, city, state, zip){
+//   this.addressStreet = addressStreet;
+//   this.city = city;
+//   this.state = state;
+//   this.zip = zip;
+// }
 
+// let birthDate = $("#born").val();
 
-
-
-
-
-
-
-
-
+// // Assembling contact from info provided
+// function Contact(fullName, phone, email, addressStreet, city, state, zip, birthDate) {
+//   this.fullName = fullName;
+//   // this.phone = phone;
+//   this.email = email;
+//   this.address = address;
+//   this.birthDate = birthDate;
+// }
 
 
 
@@ -23,9 +45,9 @@ $(document).ready(function() {
     $("#existingContacts").show();
   });
   $("#new").click(function() {
-    $("#contactList").show();
+    $("#contact-form").show();
   });
-});
 
+});
 
 
